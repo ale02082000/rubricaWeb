557f259155c0e5d3ff83801160c40d13a5610907
Alessandro Bartolucci <127590077+ale02082000@users.noreply.github.com>
2024-04-03 10:34:15 +0200

    Add files via upload

---
+++ rubricaWeb/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using rubricaWeb.Models;
+
+namespace rubricaWeb.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        StreamReader fin = new StreamReader("data/dati.csv");
+
+        List<string> lista = new();
+        lista.Add("Maurizio");
+        lista.Add("Marco");
+        lista.Add("Luca"); 
+        return View(lista);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+     public IActionResult Gelati()
+    {
+        List<Gelato> list = new List<Gelato>();
+        list.Add(new Gelato { Id = 1, Nome = "Puffo", Descrizione = "Gelato azzurro" });
+        list.Add(new Gelato { Id = 2, Nome = "Nutella", Descrizione = "Gelato al cioccolato con nutella" });
+        list.Add(new Gelato { Id = 3, Nome = "Bacio", Descrizione = "Gelato al cioccolato con nocciole" });
+        list.Add(new Gelato { Id = 4, Nome = "Fiordilatte", Descrizione = "Gelato al latte" });
+        return View(list);
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ rubricaWeb/Models/Gelato.cs
@@ -0,0 +1,16 @@
+    //
+    //
+    // progetto scritto da Montaspro Nicolò e Bartolucci Alessandro
+    //
+    //
+
+namespace rubricaWeb.Models
+{
+    public class Gelato
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public string Immagine { get; set; }
+    }
+}
